62a17a6a68351400b64820f189e97e7bdb3b383c
Mariano Iglesias <mariano@cricava.com>
2012-02-20 14:18:46 -0300

    Switching to tabs

--- Gearman.php
+++ Gearman.php
@@ -2,8 +2,8 @@
 /**
  * Lithium: the most rad php framework
  *
- * @copyright     Copyright 2012, Mariano Iglesias (http://marianoiglesias.com.ar)
- * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ * @copyright	  Copyright 2012, Mariano Iglesias (http://marianoiglesias.com.ar)
+ * @license		  http://opensource.org/licenses/bsd-license.php The BSD License
  */
 
 namespace li3_gearman;
@@ -11,111 +11,111 @@ namespace li3_gearman;
 use lithium\core\ConfigException;
 
 class Gearman extends \lithium\core\Adaptable {
-    /**
-     * Stores configurations for various authentication adapters.
-     *
-     * @var object `Collection` of authentication configurations.
-     */
-    protected static $_configurations = array();
+	/**
+	 * Stores configurations for various authentication adapters.
+	 *
+	 * @var object `Collection` of authentication configurations.
+	 */
+	protected static $_configurations = array();
 
-    /**
-     * Libraries::locate() compatible path to adapters for this class.
-     *
-     * @see lithium\core\Libraries::locate()
-     * @var string Dot-delimited path.
-     */
-    protected static $_adapters = 'adapter.queue';
+	/**
+	 * Libraries::locate() compatible path to adapters for this class.
+	 *
+	 * @see lithium\core\Libraries::locate()
+	 * @var string Dot-delimited path.
+	 */
+	protected static $_adapters = 'adapter.queue';
 
-    /**
-     * Run/schedule a job on a configuration
-     *
-     * @param string $configName Configuration to use
-     * @param string $action Action name (job) to execute
-     * @param array $args Arguments to pass to the action
-     * @param array $options Extra options (used by adapter)
-     * @return mixed Returned value by adapter's run() method
-     */
-    public static function run($configName, $action, array $args = array(), array $options = array()) {
-        $config = static::getConfig($configName);
-        $filters = $config['filters'];
-        $params = compact('action', 'args', 'configName');
-        return static::_filter(__FUNCTION__, $params,
-            function($self, $params) use($options) {
-                return $self::adapter($params['configName'])->run(
-                    $params['action'],
-                    $params['args'],
-                    array('configName' => $params['configName']) + $options
-                );
-            }
-        , $filters);
-    }
+	/**
+	 * Run/schedule a job on a configuration
+	 *
+	 * @param string $configName Configuration to use
+	 * @param string $action Action name (job) to execute
+	 * @param array $args Arguments to pass to the action
+	 * @param array $options Extra options (used by adapter)
+	 * @return mixed Returned value by adapter's run() method
+	 */
+	public static function run($configName, $action, array $args = array(), array $options = array()) {
+		$config = static::getConfig($configName);
+		$filters = $config['filters'];
+		$params = compact('action', 'args', 'configName');
+		return static::_filter(__FUNCTION__, $params,
+			function($self, $params) use($options) {
+				return $self::adapter($params['configName'])->run(
+					$params['action'],
+					$params['args'],
+					array('configName' => $params['configName']) + $options
+				);
+			}
+		, $filters);
+	}
 
-    /**
-     * Executes job on a configuration
-     *
-     * @param string $configName Configuration to use
-     * @param string $action Action name (job) to execute
-     * @param array $args Arguments to pass to the action
-     * @return mixed Returned value by adapter's handle() method
-     */
-    public static function execute($configName, $action, array $args) {
-        $config = static::getConfig($configName);
-        $filters = $config['filters'];
-        $params = compact('action', 'args');
-        return static::_filter(__FUNCTION__, $params,
-            function($self, $params) use($configName) {
-                return $self::adapter($configName)->execute(
-                    $params['action'],
-                    $params['args']
-                );
-            }
-        , $filters);
-    }
+	/**
+	 * Executes job on a configuration
+	 *
+	 * @param string $configName Configuration to use
+	 * @param string $action Action name (job) to execute
+	 * @param array $args Arguments to pass to the action
+	 * @return mixed Returned value by adapter's handle() method
+	 */
+	public static function execute($configName, $action, array $args) {
+		$config = static::getConfig($configName);
+		$filters = $config['filters'];
+		$params = compact('action', 'args');
+		return static::_filter(__FUNCTION__, $params,
+			function($self, $params) use($configName) {
+				return $self::adapter($configName)->execute(
+					$params['action'],
+					$params['args']
+				);
+			}
+		, $filters);
+	}
 
-    /**
-     * Gets the given config, checking for validity
-     *
-     * @param string $name Configuration name
-     * @return array Configuration
-     */
-    public static function getConfig($name) {
-        if (($config = static::_config($name)) === null) {
-            throw new ConfigException("Configuration {$config} has not been defined.");
-        } elseif (!is_array($config)) {
-            throw new ConfigException('Invalid configuration: not an array');
-        } elseif (empty($config['servers'])) {
-            throw new ConfigException('No servers defined. Add them to the "servers" setting');
-        }
-        return $config;
-    }
+	/**
+	 * Gets the given config, checking for validity
+	 *
+	 * @param string $name Configuration name
+	 * @return array Configuration
+	 */
+	public static function getConfig($name) {
+		if (($config = static::_config($name)) === null) {
+			throw new ConfigException("Configuration {$config} has not been defined.");
+		} elseif (!is_array($config)) {
+			throw new ConfigException('Invalid configuration: not an array');
+		} elseif (empty($config['servers'])) {
+			throw new ConfigException('No servers defined. Add them to the "servers" setting');
+		}
+		return $config;
+	}
 
-    /**
-     * Initializes configuration with default settings
-     *
-     * @param string $name The name of the configuration which is being accessed. This is the key
-     *               name containing the specific set of configuration passed into `config()`.
-     * @param array $config Contains the configuration assigned to `$name`. If this configuration is
-     *              segregated by environment, then this will contain the configuration for the
-     *              current environment.
-     * @return array Returns the final array of settings for the given named configuration.
-     */
-    protected static function _initConfig($name, $config) {
-        $defaults = array(
-            'filters' => array(),
-            'servers' => array()
-        );
-        $config = parent::_initConfig($name, $config) + $defaults;
-        foreach(array('filters', 'servers') as $arrayVar) {
-            if (!empty($config[$arrayVar]) && !is_array($config[$arrayVar])) {
-                $config[$arrayVar] = (array) $config[$arrayVar];
-            } elseif (empty($config[$arrayVar])) {
-                $config[$arrayVar] = array();
-            }
-        }
-        if (empty($config['adapter'])) {
-            $config['adapter'] = 'Job';
-        }
-        return $config;
-    }
+	/**
+	 * Initializes configuration with default settings
+	 *
+	 * @param string $name The name of the configuration which is being accessed. This is the key
+	 *				 name containing the specific set of configuration passed into `config()`.
+	 * @param array $config Contains the configuration assigned to `$name`. If this configuration is
+	 *				segregated by environment, then this will contain the configuration for the
+	 *				current environment.
+	 * @return array Returns the final array of settings for the given named configuration.
+	 */
+	protected static function _initConfig($name, $config) {
+		$defaults = array(
+			'filters' => array(),
+			'servers' => array()
+		);
+		$config = parent::_initConfig($name, $config) + $defaults;
+		foreach(array('filters', 'servers') as $arrayVar) {
+			if (!empty($config[$arrayVar]) && !is_array($config[$arrayVar])) {
+				$config[$arrayVar] = (array) $config[$arrayVar];
+			} elseif (empty($config[$arrayVar])) {
+				$config[$arrayVar] = array();
+			}
+		}
+		if (empty($config['adapter'])) {
+			$config['adapter'] = 'Job';
+		}
+		return $config;
+	}
 }
 ?>
--- extensions/adapter/queue/Job.php
+++ extensions/adapter/queue/Job.php
@@ -3,8 +3,8 @@
 /**
  * Lithium: the most rad php framework
  *
- * @copyright     Copyright 2012, Mariano Iglesias (http://marianoiglesias.com.ar)
- * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ * @copyright	  Copyright 2012, Mariano Iglesias (http://marianoiglesias.com.ar)
+ * @license		  http://opensource.org/licenses/bsd-license.php The BSD License
  */
 
 namespace li3_gearman\extensions\adapter\queue;
@@ -17,106 +17,106 @@ use GearmanClient;
  * Executes/schedules a task
  */
 class Job extends \lithium\core\Object {
-    /**
-     * Defines fully qualified name of worker, which should implement
-     * a run() method and must be hooked to the gearman server
-     *
-     * @var array
-     */
-    protected static $_classes = array(
-        'worker' => 'li3_gearman\extensions\command\Gearmand'
-    );
+	/**
+	 * Defines fully qualified name of worker, which should implement
+	 * a run() method and must be hooked to the gearman server
+	 *
+	 * @var array
+	 */
+	protected static $_classes = array(
+		'worker' => 'li3_gearman\extensions\command\Gearmand'
+	);
 
-    /**
-     * Gearman client instance
-     *
-     * @var object
-     */
-    protected $client;
+	/**
+	 * Gearman client instance
+	 *
+	 * @var object
+	 */
+	protected $client;
 
-    /**
-     * Constructor
-     *
-     * @param array $options Options
-     */
-    public function __construct(array $options) {
-        parent::__construct($options);
+	/**
+	 * Constructor
+	 *
+	 * @param array $options Options
+	 */
+	public function __construct(array $options) {
+		parent::__construct($options);
 
-        $this->client = new GearmanClient();
-        foreach($this->_config['servers'] as $server) {
-            $this->client->addServer($server);
-        }
-    }
+		$this->client = new GearmanClient();
+		foreach($this->_config['servers'] as $server) {
+			$this->client->addServer($server);
+		}
+	}
 
-    /**
-     * Run a task
-     *
-     * @param string $task Fully qualified class name, with optional method name
-     * @param array $args Arguments to pass to the task
-     * @param array $options Options:
-     *                      - boolean background: wether to run task in
-     *                      background
-     *                      - string priority: Prority. One of: low, normal,
-     *                      high
-     * @return mixed If background, job handle. Otherwise job's return value
-     */
-    public function run($task, array $args, array $options) {
-        $options = $options + array(
-            'configName' => null,
-            'background' => true,
-            'priority' => 'normal'
-        );
+	/**
+	 * Run a task
+	 *
+	 * @param string $task Fully qualified class name, with optional method name
+	 * @param array $args Arguments to pass to the task
+	 * @param array $options Options:
+	 *						- boolean background: wether to run task in
+	 *						background
+	 *						- string priority: Prority. One of: low, normal,
+	 *						high
+	 * @return mixed If background, job handle. Otherwise job's return value
+	 */
+	public function run($task, array $args, array $options) {
+		$options = $options + array(
+			'configName' => null,
+			'background' => true,
+			'priority' => 'normal'
+		);
 
-        if (empty($options['configName'])) {
-            throw new ConfigException('Missing configuration name');
-        }
+		if (empty($options['configName'])) {
+			throw new ConfigException('Missing configuration name');
+		}
 
-        if (!in_array($options['priority'], array('low', 'normal', 'high'))) {
-            throw new ConfigException("Invalid priority {$options['priority']}");
-        }
+		if (!in_array($options['priority'], array('low', 'normal', 'high'))) {
+			throw new ConfigException("Invalid priority {$options['priority']}");
+		}
 
-        if ($task[0] !== '\\') {
-            $task = '\\' . $task;
-        }
-        if (strpos($task, '::') === false) {
-            $task .= '::run';
-        }
+		if ($task[0] !== '\\') {
+			$task = '\\' . $task;
+		}
+		if (strpos($task, '::') === false) {
+			$task .= '::run';
+		}
 
-        $action = 'do';
-        switch($options['priority']) {
-            case 'low':
-                $action = $options['background'] ? 'doLowBackground' : 'doLow';
-            break;
-            case 'normal':
-                $action = $options['background'] ? 'doBackground' : 'doLow';
-            break;
-            case 'high':
-                $action = $options['background'] ? 'doHighBackground' : 'doHigh';
-            break;
-        }
+		$action = 'do';
+		switch($options['priority']) {
+			case 'low':
+				$action = $options['background'] ? 'doLowBackground' : 'doLow';
+			break;
+			case 'normal':
+				$action = $options['background'] ? 'doBackground' : 'doLow';
+			break;
+			case 'high':
+				$action = $options['background'] ? 'doHighBackground' : 'doHigh';
+			break;
+		}
 
-        $configName = $options['configName'];
-        return $this->client->{$action}(
-            static::$_classes['worker'] . '::run',
-            serialize(compact('args', 'configName', 'task'))
-        );
-    }
+		$configName = $options['configName'];
+		return $this->client->{$action}(
+			static::$_classes['worker'] . '::run',
+			serialize(compact('args', 'configName', 'task'))
+		);
+	}
 
-    /**
-     * Executes a given task with the given set of arguments.
-     * Called by li3_gearman deamon
-     *
-     * @param string $task Fully qualified task name
-     * @param array $args Arguments for the call
-     * @return mixed Returned value
-     */
-    public function execute($task, array $args = array()) {
-        if (!is_callable($task)) {
-            throw new RuntimeException("Invalid task {$task}");
-        }
+	/**
+	 * Executes a given task with the given set of arguments.
+	 * Called by li3_gearman deamon
+	 *
+	 * @param string $task Fully qualified task name
+	 * @param array $args Arguments for the call
+	 * @return mixed Returned value
+	 */
+	public function execute($task, array $args = array()) {
+		if (!is_callable($task)) {
+			throw new RuntimeException("Invalid task {$task}");
+		}
 
-        return call_user_func_array($task, $args);
-    }
+		return call_user_func_array($task, $args);
+	}
 }
 
 ?>
