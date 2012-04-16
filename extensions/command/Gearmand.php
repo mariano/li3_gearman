416e88f8bbca3f925f8a75b7da9dd28278b00390
Mariano Iglesias <mariano@cricava.com>
2012-04-16 19:03:48 -0300

    More concise output if not on verbose mode when pinging

--- extensions/command/Gearmand.php
+++ extensions/command/Gearmand.php
@@ -148,7 +148,9 @@ class Gearmand extends \lithium\console\Command {
 	 * Test that at least a worker is working
 	 */
 	public function ping($config = 'default') {
-		$this->out('Pinging... ', false);
+		if ($this->verbose) {
+			$this->out('Pinging... ', false);
+		}
 		$job = Gearman::adapter($config);
 		$job->getClient()->setTimeout(5000);
 		$result = @Gearman::run($config, 'li3_gearman\Gearman::ping', array(), array('background' => false));
